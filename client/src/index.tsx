3db3718f0f2f72a9b8c3b9e4122fdc84aab9f7c9
Mali <kul.mehmetali@yahoo.com>
2024-01-15 10:52:37 +0300

    routesback

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,76 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from "./utils/authProvider";
-import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
-import Layout from './Components/Layout'
-import Companies from './Pages/Companies';
-import Products from './Pages/Products'
-import Login from './Pages/Login';
-import Reports from './Pages/Reports';
-import { ProtectedRoute } from "./Routes/ProtectedRoute";
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Header from './Components/Header';
-import FooterComponent from './Components/Footer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-const routesForAuth = [
-  {
-    path: "/",
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: "",
-        element: <Reports />
-      }
-    ]
-  },
-  {
-    path: "/reports",
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: "",
-        element: <Reports />
-      }
-    ]
-  },
-  {
-    path: "/companies",
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: "",
-        element: <Companies />
-      }
-    ]
-  },
-  {
-    path: "/products",
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: "",
-        element: <Products />
-      }
-    ]
-  }
-
-]
-const router = createBrowserRouter([
-  {
-    path: '/login',
-    element: <Login />
-  },
-  ...routesForAuth
-])
 root.render(
   <React.StrictMode>
     <AuthProvider>
-
-      <RouterProvider router={router} />
-
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
